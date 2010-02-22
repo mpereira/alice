26b51052d7fd94714ca88f6799b58df7b14563d6
murilasso <murilo.soares3@gmail.com>
2010-02-22 11:05:55 -0300

    'sname' rather than 'name' on start.sh script

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ make
 if [[ ! -f ebin/rest_app.boot ]]; then
 	make boot
 fi
-erl -pa $PWD/ebin -pa $PWD/deps/*/ebin -name alice -s reloader -boot alice $*
+erl -pa $PWD/ebin -pa $PWD/deps/*/ebin -sname alice -s reloader -boot alice $*
